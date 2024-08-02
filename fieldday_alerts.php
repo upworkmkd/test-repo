653e51eacef8b3424965e87fcd74d29c4591b7ca
Manoj Dhiman <36550269+upworkmkd@users.noreply.github.com>
2024-08-02 22:18:35 +0530

    Add files via upload

---
+++ fieldday_alerts.php
@@ -0,0 +1,42 @@
+<div class="km_alert_message">
+    
+</div>
+<div id="km_modal" class="km_modal km_overlay">
+    <div class="km_modal_alert">
+        <a href="javascript:void();" class="km_popup_close" onclick="fieldday.closepopup(this);">
+            <svg width="12" height="12" viewport="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <line x1="1" stroke="#fff" y1="11" x2="11" y2="1" stroke-width="2"></line>
+                <line x1="1" y1="1" stroke="#fff" x2="11" y2="11" stroke-width="2"></line>
+            </svg>
+        </a>
+        <div class="km_modal_heading">
+        
+        </div>
+        <div class="km_modal_content">
+
+        </div>
+        <div class="km_modal_footer">
+
+        </div>
+    </div>
+</div>
+
+<div id="km_modal_sec" class="km_modal km_overlay">
+    <div class="km_modal_alert">
+        <a href="javascript:void();" class="km_sec_popup_close" onclick="fieldday.closeSecpopup(this);">
+            <svg width="12" height="12" viewport="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <line x1="1" stroke="#fff" y1="11" x2="11" y2="1" stroke-width="2"></line>
+                <line x1="1" y1="1" stroke="#fff" x2="11" y2="11" stroke-width="2"></line>
+            </svg>
+        </a>
+        <div class="km_modal_heading">
+        
+        </div>
+        <div class="km_modal_content">
+
+        </div>
+        <div class="km_modal_footer">
+
+        </div>
+    </div>
+</div>
